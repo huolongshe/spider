078e67029f6d9b81f6a2b8317c0bcad564346f89
huolongshe <huolongshe@126.com>
2020-07-16 16:12:37 +0800

    update readme

---
+++ pip-install-reqs.bat
@@ -0,0 +1,6 @@
+pip install -i https://pypi.douban.com/simple/ wxpython==4.0.1
+pip install -i https://pypi.douban.com/simple/ pyyaml
+pip install -i https://pypi.douban.com/simple/ requests
+pip install -i https://pypi.douban.com/simple/ numpy
+pip install -i https://pypi.douban.com/simple/ pillow
+pip install -i https://pypi.douban.com/simple/ pyinstaller
